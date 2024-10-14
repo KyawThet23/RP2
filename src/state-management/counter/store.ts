80e49658be9ce42156150b997cd53cd6df18e7b9
kyawThetHtun <kyawthe235@gmail.com>
2024-10-14 21:35:19 +0630

    state devtool

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,15 +1,21 @@
+import { mountStoreDevtool } from "simple-zustand-devtools";
 import { create } from "zustand";
 
 interface CounterStore {
   counter : number;
+  max: number;
   increase: () => void;
   reset: () => void;
 }
 
 const useCounterStore =  create<CounterStore>(set => ({
   counter: 0,
+  max: 5,
   increase: () => set(store => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 }))
+  reset: () => set(() => ({ max: 10 }))
 }));
+
+if(process.env.NODE_ENV === 'development')
+  mountStoreDevtool('Counter Store' , useCounterStore) 
 
 export default useCounterStore;
