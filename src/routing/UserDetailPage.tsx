0da7bfc8e4d07d01ca4936a9badc477a52a94f5f
kyawThetHtun <kyawthe235@gmail.com>
2024-11-03 22:03:25 +0630

    get data for current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,11 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(params);
+
   return <p>User</p>;
 };
 
